2ecb717335b4240ed52b11a87365564bf39101a5
ponomarev-vitaly <vp211082@gmail.com>
2024-10-13 23:01:45 -0500

    added new methods openBrowser, signUp, login, addToCart, logOut, CloseTheBrowser

--- src/test/java/FirstTestNGClass.java
+++ src/test/java/FirstTestNGClass.java
@@ -2,8 +2,33 @@ import org.testng.annotations.Test;
 
 public class FirstTestNGClass {
     @Test
-    public void firstTest(){
-        System.out.println("First test!");
+    public void openBrowser(){
+        System.out.println("Open the browser!");
+    }
+
+    @Test
+    public void signUp(){
+        System.out.println("Signing up...");
+    }
+
+    @Test
+    public void login(){
+        System.out.println("Login...");
+    }
+
+    @Test
+    public void addToCart(){
+        System.out.println("Add to cart...");
+    }
+
+    @Test
+    public void logOut(){
+        System.out.println("Logging out...");
+    }
+
+    @Test
+    public void closeTheBrowser(){
+        System.out.println("Closing the browser...");
     }
 
 }
